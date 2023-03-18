eccb1c15eebf172697059dab31d74579f09be226
Hallworld <prishedko.ilya@yandex.ru>
2023-03-18 14:40:53 +0400

    Initial commit

---
+++ src/main/kotlin/com/russian/desperate/gamedev/gameserver/DemoApplication.kt
@@ -0,0 +1,11 @@
+package com.russian.desperate.gamedev.gameserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
