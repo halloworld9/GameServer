e1f47ebb760ca46139565a7067042d7eb9161f86
Hallworld <prishedko.ilya@yandex.ru>
2023-05-17 20:29:58 +0400

    Make createPlayer static and update Player creation in PlayerFactory.kt

--- src/main/kotlin/com/russian/desperate/gamedev/gameserver/model/mapobjects/units/PlayerFactory.kt
+++ src/main/kotlin/com/russian/desperate/gamedev/gameserver/model/mapobjects/units/PlayerFactory.kt
@@ -2,18 +2,21 @@ package com.russian.desperate.gamedev.gameserver.model.mapobjects.units
 
 import java.util.*
 
-class PlayerFactory {
-    fun createPlayer(playerClass: PlayerClass): Player {
-        return when (playerClass) {
-            PlayerClass.WARRIOR -> Player(5, 5, 10, playerClass)
-            PlayerClass.MAGE -> Player(7, 3, 10, playerClass)
-            PlayerClass.ARCHER -> Player(7, 3, 10, playerClass)
-            PlayerClass.ASSASSIN -> Player(9, 1, 10, playerClass)
-            PlayerClass.RANDOM -> Player(0, 0, 0, playerClass)
-        }
-    }
 
-    fun createPlayer(gameClass: String): Player {
-        return createPlayer(PlayerClass.valueOf(gameClass.uppercase(Locale.getDefault())))
+class PlayerFactory {
+    companion object {
+        fun createPlayer(playerClass: PlayerClass, id: Int): Player {
+            return when (playerClass) {
+                PlayerClass.WARRIOR -> Player(5, 5, 10, playerClass, id=id)
+                PlayerClass.MAGE -> Player(7, 3, 10, playerClass, id=id)
+                PlayerClass.ARCHER -> Player(7, 3, 10, playerClass, id=id)
+                PlayerClass.ASSASSIN -> Player(9, 1, 10, playerClass, id=id)
+                PlayerClass.RANDOM -> Player(0, 0, 0, playerClass, id=id)
+            }
+        }
+
+        fun createPlayer(gameClass: String, id: Int): Player {
+            return createPlayer(PlayerClass.valueOf(gameClass.uppercase(Locale.getDefault())), id)
+        }
     }
 }
