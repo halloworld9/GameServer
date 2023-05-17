da82ef80d3ca18a264b61cbc5979934b4a6076cf
Hallworld <prishedko.ilya@yandex.ru>
2023-05-17 20:45:47 +0400

    Update cell class for wave algorithm

--- src/main/kotlin/com/russian/desperate/gamedev/gameserver/model/Cell.kt
+++ src/main/kotlin/com/russian/desperate/gamedev/gameserver/model/Cell.kt
@@ -1,15 +1,67 @@
 package com.russian.desperate.gamedev.gameserver.model
 
+import com.russian.desperate.gamedev.gameserver.model.mapobjects.structures.InteractiveObject
 import com.russian.desperate.gamedev.gameserver.model.mapobjects.structures.MapObject
-import java.io.Serializable
-
-class Cell(mapObject: MapObject) : Serializable {
-    val mapObjects = HashSet<MapObject>()
+import com.russian.desperate.gamedev.gameserver.model.mapobjects.units.Unit
+class Cell(vararg mapObjects: MapObject) {
+    private val mapObjects = HashSet<MapObject>()
+    val interactiveObjects = HashSet<InteractiveObject>()
+    var isPassable = true
+        private set
+    var hasUnit = false
+        private set
 
     init {
-        mapObjects.add(mapObject)
+        for (i in mapObjects) {
+            this.mapObjects.add(i)
+            if (!i.isPassable)
+                isPassable = false
+            else if (i is Unit)
+                hasUnit = true
+            else if (i is InteractiveObject)
+                interactiveObjects.add(i)
+        }
     }
+
     fun removeMapObject(mapObject: MapObject): Boolean {
-        return mapObjects.remove(mapObject)
+        val isRemoved = mapObjects.remove(mapObject)
+        if (isRemoved && mapObject is InteractiveObject)
+            interactiveObjects.remove(mapObject)
+        if (isRemoved && !mapObject.isPassable)
+            updateIsPassable()
+        else if (isRemoved && mapObject is Unit)
+            updateHasUnit()
+        return isRemoved
     }
+
+    fun addMapObject(mapObject: MapObject): Boolean {
+        val isAdded = mapObjects.add(mapObject)
+        if (isAdded && mapObject is InteractiveObject)
+            interactiveObjects.add(mapObject)
+        if (isAdded && !mapObject.isPassable)
+            isPassable = false
+        else if (isAdded && mapObject is Unit)
+            hasUnit = true
+
+        return isAdded
+    }
+
+    private fun updateIsPassable() {
+        for (i in mapObjects)
+            if (!i.isPassable) {
+                isPassable = false
+                return
+            }
+        isPassable = true
+    }
+
+    private fun updateHasUnit() {
+        for (i in mapObjects)
+            if (i is Unit) {
+                hasUnit = true
+                return
+            }
+        isPassable = false
+    }
+
 }
