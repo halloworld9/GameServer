e685a461342546bf4b4a32d0314c4afc02fc0c57
Hallworld <prishedko.ilya@yandex.ru>
2023-05-17 20:17:06 +0400

    Implement hashCode, equals and toString methods in User.kt

--- src/main/kotlin/com/russian/desperate/gamedev/gameserver/model/dto/User.kt
+++ src/main/kotlin/com/russian/desperate/gamedev/gameserver/model/dto/User.kt
@@ -1,7 +1,22 @@
 package com.russian.desperate.gamedev.gameserver.model.dto
 
-import java.io.Serializable
 
-class User(var name: String): Serializable {
+class User(var name: String) {
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as User
+
+        return name == other.name
+    }
+
+    override fun hashCode(): Int {
+        return name.hashCode()
+    }
+
+    override fun toString(): String {
+        return name
+    }
 }
