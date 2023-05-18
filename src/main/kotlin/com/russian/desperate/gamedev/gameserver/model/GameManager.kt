ecebff2ec2ee73f88310cdd2eae9a06f727d34a9
Hallworld <prishedko.ilya@yandex.ru>
2023-05-18 20:32:00 +0400

    Add basic logic to GameManager.kt

--- src/main/kotlin/com/russian/desperate/gamedev/gameserver/model/GameManager.kt
+++ src/main/kotlin/com/russian/desperate/gamedev/gameserver/model/GameManager.kt
@@ -1,23 +1,86 @@
 package com.russian.desperate.gamedev.gameserver.model
 
+import com.russian.desperate.gamedev.gameserver.model.actions.Action
 import com.russian.desperate.gamedev.gameserver.model.dto.User
+import com.russian.desperate.gamedev.gameserver.model.exceptions.NoSuchUserException
+import com.russian.desperate.gamedev.gameserver.model.mapobjects.structures.InteractiveObject
+import com.russian.desperate.gamedev.gameserver.model.mapobjects.units.Player
+import com.russian.desperate.gamedev.gameserver.model.mapobjects.units.PlayerClass
+import com.russian.desperate.gamedev.gameserver.model.mapobjects.units.PlayerFactory
 
 class GameManager {
     private val lobby = Lobby()
-    private var game: Game? = null
+    private lateinit var game: Game
+    private val userPlayerMap = HashMap<User, Player>()
+
+    fun startGame() {
+        if (!this::game.isInitialized) {
+            for ((id, i) in lobby.getUsersAndClasses().withIndex()) {
+                val user = i.first
+                val player = PlayerFactory.createPlayer(i.second, id)
+                userPlayerMap[user] = player
+            }
+            game = Game(userPlayerMap.values)
+        }
+    }
+
+    fun movePlayer(user: User, x: Int, y: Int): Boolean {
+        val player = userPlayerMap[user] ?: throw NoSuchUserException(user)
+        game.movePlayer(player, x, y)
+        return true
+    }
+
+    fun getInteractiveObjects(user: User): Set<InteractiveObject> {
+        val player = userPlayerMap[user] ?: throw NoSuchUserException(user)
+        return game.getInteractiveObjects(player)
+    }
+
+    fun activateInteractiveObject(user: User, interactiveObject: InteractiveObject): Action {
+        val player = userPlayerMap[user] ?: throw NoSuchUserException(user)
+        return game.activateInteractiveObject(player, interactiveObject)
+    }
+
+    fun getPath(user: User, x: Int, y: Int): ArrayList<Pair<Int, Int>> {
+        val player = userPlayerMap[user] ?: throw NoSuchUserException(user)
+        return game.getPlayersPath(player, x, y)
+    }
+
+    fun getPlayerMoves(user: User): Array<Array<Int>> {
+        val player = userPlayerMap[user] ?: throw NoSuchUserException(user)
+        return game.getPlayerMoves(player)
+    }
+
+    fun startTurn(user: User): Player {
+        val player = userPlayerMap[user] ?: throw NoSuchUserException(user)
+        return game.startTurn(player)
+    }
+
+    fun endTurn(user: User) {
+        val player = userPlayerMap[user] ?: throw NoSuchUserException(user)
+        game.endTurn(player)
+    }
 
     fun addUser(user: User): Boolean {
         return lobby.addUser(user)
+    }
+
+    fun setPlayerClass(user: User, playerClass: PlayerClass): Boolean {
+        return lobby.setPlayerClass(user, playerClass)
     }
 
     fun removeUser(user: User): Boolean {
         return lobby.removeUser(user)
     }
 
-    fun startGame() {
-        if (game == null) {
-            game = Game(lobby.getUsersAndClasses())
-        }
+    fun getPlayers(): ArrayList<String> {
+        return lobby.getUsernames()
+    }
+
+    fun containsUser(user: User): Boolean {
+        return lobby.containsUser(user)
+    }
+    override fun toString(): String {
+        return "GameManager(lobby=$lobby, game=$game)"
     }
 
 }
