7a29201734d98cf45f1495f25d9e9718025ed2d8
Hallworld <prishedko.ilya@yandex.ru>
2023-07-08 22:57:37 +0400

    Change logic in InteractiveObject.kt

--- src/main/kotlin/com/russian/desperate/gamedev/gameserver/model/mapobjects/structures/InteractiveObject.kt
+++ src/main/kotlin/com/russian/desperate/gamedev/gameserver/model/mapobjects/structures/InteractiveObject.kt
@@ -1,11 +1,11 @@
 package com.russian.desperate.gamedev.gameserver.model.mapobjects.structures
 
-import com.russian.desperate.gamedev.gameserver.model.actions.Action
 import com.russian.desperate.gamedev.gameserver.model.mapobjects.units.Player
 
 
 interface InteractiveObject : MapObject {
 
-    fun interact(player: Player): Action
+    fun interact(player: Player)
+    fun getInteractDescription(): String
 
 }
