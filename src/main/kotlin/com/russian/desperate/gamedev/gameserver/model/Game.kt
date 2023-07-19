75803714b543d1c948675e43b5171dd317681e5d
Hallworld <prishedko.ilya@yandex.ru>
2023-07-19 14:52:46 +0400

    Fix bug with first turn moves.

--- src/main/kotlin/com/russian/desperate/gamedev/gameserver/model/Game.kt
+++ src/main/kotlin/com/russian/desperate/gamedev/gameserver/model/Game.kt
@@ -26,6 +26,7 @@ class Game(playerClasses: Collection<Player>) {
         }
 
         initPlayerMoves()
+        updatePlayersMoveMatrix(playerOrder[0])
     }
 
     private fun initPlayerMoves() {
